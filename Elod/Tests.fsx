baca51bf7252a71dbd46e20ca952f71af75ebd4f
htoma <horia.toma@availpro.com>
2016-06-29 17:21:06 +0200

    Upsert and some tests

--- Elod/Tests.fsx
+++ Elod/Tests.fsx
@@ -11,5 +11,25 @@ tree.AddEntry(3, "Three")
 tree.AddEntry(1, "One")
 tree.AddEntry(5, "Five")
 
-tree.GetValue 7
+tree.GetValue 5 // Some "Five"
+tree.GetValue 7 // None
 
+let threeWaySelector (nodeValue:int,value:int) =
+    if value<=nodeValue/2 then 0
+    else if value<=nodeValue then 1
+    else 2
+        
+let ttree = Tree(3, threeWaySelector)
+ttree.AddEntry(5, "Five")
+ttree.AddEntry(7, "Seven")
+ttree.AddEntry(4, "Four")
+ttree.AddEntry(9, "Nine")
+ttree.AddEntry(12, "Twelve")
+ttree.AddEntry(1, "One")
+ttree.AddEntry(5, "Five more", true)
+
+ttree.GetValue 1    
+ttree.GetValue 2
+ttree.GetValue 9
+ttree.GetValue 10
+ttree.GetValue 5
--- Elod/Tree.fs
+++ Elod/Tree.fs
@@ -2,28 +2,34 @@
 
 type Node<'a,'b> =
     { Key: 'a
-      Value: 'b
+      mutable Value: 'b //value can be updated for an existing key
       Nodes: Node<'a,'b> option array }
 
 type Tree<'a,'b> when 'a : equality (childNodes:int, childSelector: ('a*'a) ->int) =
     let mutable Root: Node<'a,'b> option = None
    
-    member __.AddEntry(key:'a,value:'b) =
-        let newNode = {Key=key; Value=value; Nodes= Array.replicate childNodes None} |> Some
+    member __.AddEntry(key:'a,value:'b,?upsert:bool) =
+        let newNode = {Key=key; Value=value; Nodes= Array.replicate childNodes None} |> Some //precreate the node to be added
         match Root with
         | None ->
             Root <- newNode
         | Some root ->
             let rec findLeaf(node: Node<'a,'b>) =
-                let pos = childSelector(root.Key, key)
-                if pos >= childNodes
-                    then failwith (sprintf "Selector returned index %i larger than limit %i" pos childNodes)
+                match node.Key,upsert with //check on key node and the flag for insert/update
+                | sameKey,Some flag when sameKey=key && flag=true -> 
+                    printfn "Updating an existing key: %A with %A" key value
+                    node.Value<-value
+                | _ ->    
+                    //it's all insert
+                    let pos = childSelector(root.Key, key)
+                    if pos >= childNodes
+                        then failwith (sprintf "Selector returned child index %i larger than limit %i" pos childNodes)
 
-                match node.Nodes.[pos] with
-                | None ->
-                    node.Nodes.[pos]<-{Key=key; Value=value; Nodes= Array.replicate childNodes None} |> Some
-                | Some child ->
-                            findLeaf child
+                    match node.Nodes.[pos] with
+                    | None ->
+                        node.Nodes.[pos]<-{Key=key; Value=value; Nodes= Array.replicate childNodes None} |> Some
+                    | Some child ->
+                                findLeaf child
                 
             findLeaf root
 
